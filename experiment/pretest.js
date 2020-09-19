1b9f5fbeaf447a2d57d6c206d63b54a2a970d679
starxtony <47916074+starxtony@users.noreply.github.com>
2020-09-19 13:35:50 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -90,23 +90,23 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "Which of the following is having maximum rate of heat transfer?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "Steel Rod",                  ///// Write the option 1 inside double quotes
+        b: "Copper Rod",                  ///// Write the option 2 inside double quotes
+        c: "Heat Pipe",                  ///// Write the option 3 inside double quotes
+        d: "Aluminium Rod"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
     },
 
     {
-     question: "This is question 2?",  ///// Write the question inside double quotes
+     question: "Which of the following convection process has highest value of heat transfer coefficient?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "Free convection",                  ///// Write the option 1 inside double quotes
+        b: "Forced Convection",                  ///// Write the option 2 inside double quotes
+        c: "Boiling and Condensation",                  ///// Write the option 3 inside double quotes
+        d: "Mixed Convection"                   ///// Write the option 4 inside double quotes
       },
       correctAnswer: "b"                ///// Write the correct option inside double quotes
     },                                  ///// To add more questions, copy the section below 
